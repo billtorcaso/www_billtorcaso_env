c11f87bdbaac3f9faaca08992f96c42fb11fede5
Bill Torcaso <bill@billtorcaso.org>
2020-05-06 15:12:03 -0400

    Interim commit; the things in 'navigation_tags.py' _almost_ work.

---
+++ BTOPage/templatetags/navigation_tags.py
@@ -0,0 +1,74 @@
+# https://docs.djangoproject.com/en/1.9/howto/custom-template-tags/
+from django import template
+from wagtail.core.models import Page
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('BTOPage/page_objects_count.html', takes_context=True)
+def page_objects_count(context):
+
+    page_objects_count = Page.objects.count() - 1  # Ignore the ROOT page, it is not renderable
+
+    return {
+        'page_objects_count': page_objects_count,
+    }
+
+
+@register.inclusion_tag('BTOPage/page_objects_all.html', takes_context=True)
+def page_objects_all(context):
+
+    page_objects_all = [thing for thing in Page.objects.all() if thing.title != "Root"]
+
+    return {
+        'page_objects_all': page_objects_all,
+    }
+
+
+
+@register.inclusion_tag('footer.html', takes_context=True)
+def footer_text_get(context):
+    ###footer_text = ""
+    ###if FooterText.objects.first() is not None:
+        ###footer_text = FooterText.objects.first().body
+    footer_text="Hello, world! I am some hard-wired FooterText"
+
+    return {
+        'footer_text': footer_text,
+    }
+
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    # This returns a core.Page. The main menu needs to have the site.root_page
+    # defined else will return an object attribute error ('str' object has no
+    # attribute 'get_children')
+    return context['request'].site.root_page
+
+
+def has_children(page):
+    # Generically allow index pages to list their children
+    return page.get_children().live().exists()
+
+
+def is_active(page, current_page):
+    # To give us active state on main navigation
+    return (current_page.url_path.startswith(page.url_path) if current_page else False)
+
+
+@register.inclusion_tag('tags/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context):
+    self = context.get('self')
+    if self is None or self.depth <= 2:
+        # When on the home page, displaying breadcrumbs is irrelevant.
+        ancestors = ()
+    else:
+        ancestors = Page.objects.ancestor_of(
+            self, inclusive=True).filter(depth__gt=1)
+    return {
+        'ancestors': ancestors,
+        'request': context['request'],
+    }
+
+
