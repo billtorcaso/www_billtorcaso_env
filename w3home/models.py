6497016545d0485aeb4ef80f21fa019b7dbf9044
Bill Torcaso <bill@billtorcaso.org>
2020-05-31 14:03:46 -0400

    Add initial migration for app 'w3home'.

--- w3home/models.py
+++ w3home/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core.fields import RichTextField
+from wagtail.core import blocks
+
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+
+from wagtail.images.models import Image
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+
+class W3HomePage(Page):
+    background_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    author = models.CharField(max_length=64, blank=True, null=True)
+    body = StreamField(
+        [
+            ("heading", blocks.CharBlock(classname="full title")),
+            ("rtf", blocks.RichTextBlock()),
+            ("image", ImageChooserBlock()),
+        ]
+    )
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel("background_image"),
+        FieldPanel("author", classname="full"),
+        StreamFieldPanel("body", classname="full"),
+    ]
